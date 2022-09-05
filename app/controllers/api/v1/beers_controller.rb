945a066100a9b07b62edd84575219a9012e88d1d
afeez oloyede <oloyedeafeez@363photography.org>
2022-09-04 22:06:55 -0700

    get a list of all beers in index page

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -3,7 +3,26 @@ require 'rest-client'
 class Api::V1::BeersController < ApplicationController
 
   def index
-    beers = [{'identifier' => 1, 'name' => 'Lager', 'description' => 'best beer ever' }]
+    result = make_beer_request
+    beers = form_json_object(result)
     render json: beers
   end
+
+  private
+
+  def make_beer_request
+    url = 'https://api.punkapi.com/v2/beers'
+    response = RestClient.get(url)
+    JSON.parse(response)
+  end
+
+  def form_json_object(result)
+    arr = []
+    myhash = {}
+    result.each do |beer|
+      arr << myhash = { 'identifier' => beer['id'], 'name' => beer['name'], 'description' => beer['description'] }
+    end
+    arr
+  end
+
 end
