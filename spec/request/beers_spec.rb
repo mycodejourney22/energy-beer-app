181c4cefad8733846521490d5875e0b5f40cdc57
afeez oloyede <oloyedeafeez@363photography.org>
2022-09-04 21:17:42 -0700

    testing routes and http status for index action

--- spec/request/beers_spec.rb
+++ spec/request/beers_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::BeersController,  :type => :controller do
-
+  describe 'GET/index' do
+    it 'test http status' do
+      get :index, params: { use_route: '/api/v1/beers/'}
+      expect(response.status).to eql(200)
+    end
+  end
 end
