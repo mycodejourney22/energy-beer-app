d792d11afa5685172fa723d65d1a42cc67f1b515
afeez oloyede <oloyedeafeez@363photography.org>
2022-09-04 21:46:09 -0700

    test that hash key exactly match (hardcorded)

--- spec/request/beers_spec.rb
+++ spec/request/beers_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::BeersController,  :type => :controller do
   describe 'GET/index' do
+
     it 'test http status' do
       get :index, params: { use_route: '/api/v1/beers/'}
       expect(response.status).to eql(200)
+    end
+
+    it 'test exact keys of response object' do
+      get :index, params: { use_route: '/api/v1/beers/' }
+      hash_keys = ['identifier', 'name', 'description']
+      json = JSON.parse(response.body)
+      json.each do |hash|
+        expect(hash.keys).to contain_exactly(*hash_keys)
+      end
     end
   end
 end
