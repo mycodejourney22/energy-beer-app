675617ef52d271bc5d2c2bf8426748ba38e1b5f0
afeez oloyede <oloyedeafeez@363photography.org>
2022-09-04 21:31:18 -0700

    routes and http response okay for index action

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::BeersController < ApplicationController
+
+  def index
+    beers = [{'identifier' => 1, 'name' => 'Lager', 'description' => 'best beer ever' }]
+    render json: beers
+  end
 end
