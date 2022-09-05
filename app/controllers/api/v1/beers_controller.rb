aa8bb79f6539f5c22a51344c82398136a860f185
afeez oloyede <oloyedeafeez@363photography.org>
2022-09-04 23:21:36 -0700

    refactor api beer controller

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -1,29 +1,24 @@
 require 'rest-client'
 
 class Api::V1::BeersController < ApplicationController
-
   def index
-    params[:query].present? ? url = build_url_with_params(params[:query]) : url = build_url
-    result = make_beer_request(url)
-    beers = form_json_object(result)
+    beers = present_beer_index_request
     render json: beers
   end
 
   def show
-    url = build_url_for_show(params[:id])
-    result = make_beer_request(url)
-    beer = form_json_object(result)
+    beer = present_beer_show_request(params[:id])
     render json: beer
   end
 
   private
 
-  def build_url
-    'https://api.punkapi.com/v2/beers'
-  end
-
-  def build_url_with_params(params)
-    "https://api.punkapi.com/v2/beers?beer_name=#{params}"
+  def build_url_for_index
+    if params[:query].present?
+      "https://api.punkapi.com/v2/beers?beer_name=#{params[:query]}"
+    else
+      'https://api.punkapi.com/v2/beers'
+    end
   end
 
   def build_url_for_show(id)
@@ -44,4 +39,15 @@ class Api::V1::BeersController < ApplicationController
     arr
   end
 
+  def present_beer_index_request
+    url = build_url_for_index
+    result = make_beer_request(url)
+    form_json_object(result)
+  end
+
+  def present_beer_show_request(id)
+    url = build_url_for_show(id)
+    result = make_beer_request(url)
+    form_json_object(result)
+  end
 end
